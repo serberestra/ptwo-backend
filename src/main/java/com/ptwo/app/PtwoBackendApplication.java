09aefb9659af88c3de87130037180b322b17912c
serberestra <serberestra@gmail.com>
2020-06-05 17:40:02 -0400

    First Commit - Refresh Base Project

---
+++ src/main/java/com/ptwo/app/PtwoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ptwo.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtwoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtwoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ptwo/app/PtwoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ptwo.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtwoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
