18f8f6ea212082657267d309f8cd0d1f0a1dd5bd
serberestra <serberestra@gmail.com>
2020-06-04 21:23:02 -0400

    Project converted to Eclipse

---
+++ JavaBackend/bin/target/classes/data.sql
@@ -0,0 +1,3 @@
+insert into company(id, name, phone_number, address) values(5001, 'ABC Pressure Gutter', '(808)123', '120 Ruby St.');
+insert into company(id, name, phone_number, address) values(5002, 'XYZ Electronics', '(565)345', '150 Silver St.');
+insert into worker(id, first_name, last_name, company_id, service_name) values(1001,'Rich', 'Black', 5001, 'Gutter Clean');
