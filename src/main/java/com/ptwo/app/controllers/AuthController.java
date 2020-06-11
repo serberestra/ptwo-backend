cb06dd749351af2dd0e8a32a9f764acfd4b37ab0
serberestra <serberestra@gmail.com>
2020-06-11 13:42:39 -0400

    Testing docker service.

--- src/main/java/com/ptwo/app/controllers/AuthController.java
+++ src/main/java/com/ptwo/app/controllers/AuthController.java
@@ -2,6 +2,7 @@ package com.ptwo.app.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,6 +32,11 @@ public class AuthController {
 	public User authUser(@RequestBody User incomingUser) {
 		System.out.println(incomingUser);
 		return userService.findUserByUsernameAndPassword(incomingUser.getUsername(), incomingUser.getPassword());
+	}
+	
+	@GetMapping("/test")
+	public String testConnection() {
+		return "Connection OK to port 9191 - Endpoint Enable";
 	}
 
 }
