fe9ed28c1e0c59dc53dcce5fc2d43cfe5a756803
serberestra <serberestra@gmail.com>
2020-06-10 23:02:10 -0400

    Add CrossOrigin to authentication controller.

--- src/main/java/com/ptwo/app/controllers/AuthController.java
+++ src/main/java/com/ptwo/app/controllers/AuthController.java
@@ -1,6 +1,7 @@
 package com.ptwo.app.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import com.ptwo.app.model.User;
 import com.ptwo.app.service.impl.UserServiceImpl;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api")
 public class AuthController {
 	
@@ -27,6 +29,7 @@ public class AuthController {
 	@PostMapping("/auth")
 	@ResponseBody
 	public User authUser(@RequestBody User incomingUser) {
+		System.out.println(incomingUser);
 		return userService.findUserByUsernameAndPassword(incomingUser.getUsername(), incomingUser.getPassword());
 	}
 
